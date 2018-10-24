655607a98439be1460697e73ce53d3295a46b0df
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2018-10-23 22:06:00 -0600

    10/23 End Of DAY
    Lay out of Landing page done. General FLOW completed

---
+++ SnowG/app.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+function myMap() {
+  var mapOptions = {
+      center: new google.maps.LatLng(51.5, -0.12),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+  }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
